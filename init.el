3ffd33060e99a569c8d881674d8ba79d4dc81fe8
blacky <hourou.hamurinzu@gmail.com>
2016-02-06 18:09:45 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,30 @@
+;; バックアップファイルを作成しない
+(setq make-backup-files nil)
+
+;; スタートアップ画面を表示しない
+(setq inhibit-startup-message t)
+
+;;; load-pathを追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+     (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+             (normal-top-level-add-subdirs-to-load-path))))))
+
+;;; ディレクトリをサブディレクトリごとload-pathに追加
+(add-to-load-path "elisp")
+
+;; Haskell-Mode
+(require 'haskell-mode)
+(require 'haskell-cabal)
+
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+
+;; zencoding-mode
+(require 'zencoding-mode)
+(add-hook 'sgml-mode-hook 'zencoding-mode)
+(define-key zencoding-mode-keymap (kbd "C-i") 'zencoding-expand-line)
