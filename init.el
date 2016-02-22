ea324bdb6318ce55ba7b6f9e8e80adf825030c98
nomad blacky <coffee.2milk.2suger@gmail.com>
2016-02-22 16:14:45 +0900

    Cygwin環境でTABが効かない問題を修正した。

--- init.el
+++ init.el
@@ -33,5 +33,4 @@
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent はスペース2個
 (eval-after-load "emmet-mode"
   '(define-key emmet-mode-keymap (kbd "C-j") nil)) ;; C-j は newline のままにしておく
-(keyboard-translate ?\C-i ?\H-i) ;;C-i と Tabの被りを回避
-(define-key emmet-mode-keymap (kbd "H-i") 'emmet-expand-line) ;; C-i で展開
+(define-key emmet-mode-keymap (kbd "C-i") 'emmet-expand-line) ;; C-i で展開
