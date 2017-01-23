0e255289ff9b92479d982a6392d19ffdee14db41
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-01-23 17:40:46 +0900

    Fix desktop settings

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
 (setq x-select-enable-clipboard t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Package Specific Settings
+;; Mode / Package Specific Settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; recentf-ext
@@ -264,6 +264,10 @@
 ;; anzu
 (global-anzu-mode +1)
 
+;; desktop
+(desktop-save-mode t)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Key Bindings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -320,19 +324,3 @@
       (tool-bar-mode 0)
       (add-to-list 'default-frame-alist '(font . "Migu 1M-10"))
       ))
-
-;; custom-set-variables
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(desktop-save-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'upcase-region 'disabled nil)
-
