ab12c0d42fe498cd450b6bf9ce8b914528a2f626
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-12-13 10:50:50 +0900

    Fix web-mode settings

--- init.el
+++ init.el
@@ -168,8 +168,11 @@
 ;; web-mode
 (defun web-mode-hook ()
   (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
   (add-hook 'web-mode-hook 'web-mode-hook))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.page\\'" . web-mode))
 
 ;; scala-mode
 (require 'scala-mode)
