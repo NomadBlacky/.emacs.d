5cfe2241a6a68708f8f5449935ad8be51690fda4
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-02-16 15:18:59 +0900

    Fix: web-mode configurations
    - Indents
    - Paddings

--- init.el
+++ init.el
@@ -174,9 +174,15 @@
 
 ;; web-mode
 (defun web-mode-hook ()
-  (setq web-mode-markup-indent-offset 4)
+  "My web-mode hook."
+  ;; offsets
+  (setq web-mode-markup-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
+  ;; paddings
+  (setq web-mode-script-padding 2)
+  (setq web-mode-style-padding 2)
+  
   (add-hook 'web-mode-hook 'web-mode-hook))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.page\\'" . web-mode))
