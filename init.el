1999f2e49d7babd333244b0583b51dc5903c91d1
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-05-15 15:07:12 +0900

    Fix to use xsel when launch on linux

--- init.el
+++ init.el
@@ -69,15 +69,16 @@
 (key-chord-define-global "sr" 'create-scratch-buffer)
 
 ;; Sync X clipboard
-(setq interprogram-paste-function
-      (lambda ()
-         (shell-command-to-string "xsel -b -o")))
-(setq interprogram-cut-function
-      (lambda (text &optional rest)
-         (let* ((process-connection-type nil)
-                (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
-           (process-send-string proc text)
-           (process-send-eof proc))))
+(when (eq system-type 'gnu/linux)
+  (setq interprogram-paste-function
+        (lambda ()
+          (shell-command-to-string "xsel -b -o")))
+  (setq interprogram-cut-function
+        (lambda (text &optional rest)
+          (let* ((process-connection-type nil)
+                 (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
+            (process-send-string proc text)
+            (process-send-eof proc)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Mode / Package Specific Settings
