0c1c8cbd082caa3f0e8c9edb675aacee42bbc74f
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-04-15 10:54:34 +0900

    Fix OSX settings

--- init.el
+++ init.el
@@ -480,15 +480,7 @@
 
 ;; for Mac
 (when (eq system-type 'darwin)
-  (setq ns-command-modifier (quote meta))
-  (defun copy-from-osx ()
-    (shell-command-to-string "pbpaste"))
-  (defun paste-to-osx (text &optional push)
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc)))
-  (setq interprogram-cut-function 'paste-to-osx)
-  (setq interprogram-paste-function 'copy-from-osx))
+  (setq ns-command-modifier (quote meta)))
 
 ;; for GUI
 (if window-system
